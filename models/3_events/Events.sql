2a8ccf5e2ac042e028d133a2e3c1ea526fca3225
JamesLiSooeyUiPath <james.lisooey@uipath.com>
2022-06-28 12:59:04 +0000

    Update Events.sql

--- models/3_events/Events.sql
+++ models/3_events/Events.sql
@@ -8,12 +8,12 @@ Template for Events file
 Update Required: Add required fields -- Activity and Event End
 Update Required: Add optional fields for event based attributes
 */
-Cases as (
+Events as (
     select
         Customer_Data_Input."OR_ID" + '_' + Customer_Data_Input."Client_Name" as "Case ID"
     from Customer_Data_Input
 )
 
-select * from Cases
+select * from Events
 
 
